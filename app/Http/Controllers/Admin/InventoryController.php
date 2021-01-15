2235289beebe5826200db71cd342e1759616b6da
ferk28 <31743730+ferk28@users.noreply.github.com>
2021-01-14 18:47:13 -0600

    Update InventoryController.php

--- app/Http/Controllers/Admin/InventoryController.php
+++ app/Http/Controllers/Admin/InventoryController.php
@@ -25,7 +25,7 @@ class InventoryController extends Controller
      */
     public function index()
     {
-        $inventories = Inventory::paginate(10);
+        $inventories = Inventory::orderBy('id','desc')->paginate(10);
         return view('inventories.index', compact('inventories'));
     }
 
